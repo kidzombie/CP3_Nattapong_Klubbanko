f4e54415ec9e3a32edb87c4317ddd49f4b52a6b3
kidzombie <kidzombie_007@hotmail.com>
2022-07-13 15:11:12 +0700

    add lecture 29

---
+++ lecture29_AssignmentOperatorExample.py
@@ -0,0 +1,20 @@
+a = 10
+b = 12
+c = a + b
+a += 5
+print(a)
+
+money = 1000
+money = money - 50
+money = money - 20
+print(money)
+
+moneyA = 1000
+moneyA -= 50
+moneyA -= 20
+print(moneyA)
+
+moneyB = 1000
+moneyB = - 50
+moneyB = - 20
+print(moneyB)
