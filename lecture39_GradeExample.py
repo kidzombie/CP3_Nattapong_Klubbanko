b97e685c47d94472d1bcf5da4109166f6d477236
kidzombie <kidzombie_007@hotmail.com>
2022-07-13 18:43:38 +0700

    add lecture 39

---
+++ lecture39_GradeExample.py
@@ -0,0 +1,17 @@
+score = int(input("Input Score :"))
+if score >= 80:
+    print("Your grade is A")
+elif score >= 75:
+    print("Your grade is B+")
+elif score >= 70:
+    print("Your grade is B")
+elif score >= 65:
+    print("Your grade is C+")
+elif score >= 60:
+    print("Your grade is C")
+elif score >= 55:
+    print("Your grade is D+")
+elif score >= 50:
+    print("Your grade is D")
+else:
+    print("Your grade is F")
