a8d0b8f3bc1ecd6f993b1615eed703f2fe327f0b
kidzombie <kidzombie_007@hotmail.com>
2022-07-13 22:43:43 +0700

    add lecture 43

---
+++ lecture43_WhileExample.py
@@ -0,0 +1,11 @@
+correctNumber = 17
+userGuess = 0
+while userGuess != correctNumber:
+    userGuess = int(input("Please guess a number :"))
+    if userGuess > correctNumber:
+        print("To Large")
+    elif userGuess < correctNumber:
+        print("Too Small")
+    elif userGuess == correctNumber:
+        print("Correct!!")
+
