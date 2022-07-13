ae479d7ecfadfca8a4a8c38ad1a856076e2b1afd
kidzombie <kidzombie_007@hotmail.com>
2022-07-13 16:03:53 +0700

    add lecture 32

---
+++ lecture32_InputDataExample.py
@@ -0,0 +1,11 @@
+name = input("First name :")
+lastName = input("Last name :")
+print("Hello", name, lastName)
+
+
+x = input("Number 1:")
+y = input("Number 2:")
+z = int(x)+int(y)
+print("Number 1 + Number 2 =", z)
+print(x, "+", y, "=", z)
+
