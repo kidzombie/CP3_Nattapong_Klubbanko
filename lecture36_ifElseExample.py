9ee43d152e4b5621a48d8604f2b40d122e2b2be3
kidzombie <kidzombie_007@hotmail.com>
2022-07-13 17:28:23 +0700

    add lecture 36

---
+++ lecture36_ifElseExample.py
@@ -0,0 +1,16 @@
+if False:
+    print("Hello Condition 1 !")
+    print("Hello Condition 2 !")
+
+
+if False:
+    print("Hello Condition 3 !")
+print("Hello Condition 4 !")
+
+
+money = 1000
+if money > 300:
+    print("Go to KFC!!")
+else:
+    print("So Sad!! T_T")
+print("Bye Bye")
