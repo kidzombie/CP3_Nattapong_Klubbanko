c84856e2f94ad3494718cd277eea5d965e82c982
kidzombie <kidzombie_007@hotmail.com>
2022-07-15 15:56:34 +0700

    add lecture 50

---
+++ lecture50_addNumberExample.py
@@ -0,0 +1,17 @@
+def addNumber(x, y):
+    print(x+y)
+def delNumber(x, y):
+    print(x-y)
+def mulNumber(x, y):
+    print(x*y)
+def divNumber(x, y):
+    print(x/y)
+
+addNumber(10, 2)
+delNumber(10, 2)
+mulNumber(10, 2)
+divNumber(10, 2)
+
+
+
+
