55644800174361471907bed3e18bc861f0bddd18
kidzombie <kidzombie_007@hotmail.com>
2022-07-15 23:42:20 +0700

    add lecture 63

---
+++ lecture63_ListExample.py
@@ -0,0 +1,9 @@
+list1 = [0,2,4,6,8]
+print(range(5))
+for i in list1:
+    print(i)
+
+print(list1)
+
+customerList = ["Monkey Luffy","Hiruma Yoichi","Kozuki Oden", "Portgas Ace"]
+print(customerList[:3])
