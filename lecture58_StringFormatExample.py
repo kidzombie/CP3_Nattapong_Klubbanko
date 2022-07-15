b87e3ee8831715e3d91a161d6417d2061f1fcdb8
kidzombie <kidzombie_007@hotmail.com>
2022-07-15 22:22:57 +0700

    add lecture 58

---
+++ lecture58_StringFormatExample.py
@@ -0,0 +1,8 @@
+name ="First"
+weight  = "66"
+
+print("Hello " + name + "! and weight is " + weight + "kg")
+print("Hello %s! and weight is %dkg" % (name,66))
+print("Hello %s! and weight is %fkg" % (name,66.66))
+
+
