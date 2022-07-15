1b1b26c34d3d389ecfa541e6540e438c9a34fadc
kidzombie <kidzombie_007@hotmail.com>
2022-07-16 00:27:27 +0700

    add lecture 67

---
+++ lecture67_TupleFunctionExample.py
@@ -0,0 +1,8 @@
+tupleEx = (1,200,3,4,10,4,4)
+listEx = [1,200,3,4,10]
+print(len(listEx))
+print(max(listEx))
+print(max(tupleEx))
+print(tuple(listEx))
+print(tupleEx.index(200))
+print(tupleEx.count(4))
