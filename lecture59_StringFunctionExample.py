f99fbf626f5f7d3a833bc389fadb783ed61f6e99
kidzombie <kidzombie_007@hotmail.com>
2022-07-15 23:14:19 +0700

    add lecture 59

---
+++ lecture59_StringFunctionExample.py
@@ -0,0 +1,8 @@
+name = input("Firstname :").capitalize()
+lastname = input("Lastname :").capitalize()
+print("Hello !! %s %s !"%(name, lastname))
+
+text = "First"
+textFormatted = "Wecome %s"%(name)
+print(textFormatted.center(21,"-"))
+print(len(name))
