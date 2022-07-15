fbc08e8f1d5c935dee54f8fe08f8f2dc8dc8edd5
kidzombie <kidzombie_007@hotmail.com>
2022-07-15 23:57:14 +0700

    add lecture 64

---
+++ lecture64_ListFunctionExample.py
@@ -0,0 +1,12 @@
+myFriends = ['Prame','Aces','Oden']
+print(myFriends)
+myFriends[2] = "Odun"
+print(myFriends)
+myFriends.append('Bank')
+print(myFriends)
+myFriends.remove('Aces')
+print(myFriends)
+del myFriends[0]
+print(myFriends)
+
+
