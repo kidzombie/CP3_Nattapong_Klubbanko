2fe13409107860cbe654568734488a3de5195da7
kidzombie <kidzombie_007@hotmail.com>
2022-07-17 11:31:21 +0700

    add lecture 70

---
+++ lecture70_DictionaryFunctionExample.py
@@ -0,0 +1,8 @@
+words = {"Bird":"นก","Ant":"มด","cat": "แมว","Dog":"สุนัุข","Egg":"ไข่"}
+print(words["Bird"])
+print(words.items())
+print(words.keys())
+for i in words.keys():
+    print(i)
+for j in words.values():
+    print(j)
